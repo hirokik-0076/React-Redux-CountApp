817e209572f54e281b76287c6a2d04d4114be593
Hiroki Kinoshita <hiroki.kinoshita@sun-asterisk.com>
2019-05-17 10:12:11 +0700

    review-change

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,10 @@
-export const TEXTCHANGE = 'TEXTCHANGE';
+export const TEXT_CHANGE = 'TEXTCHANGE';
 
 //Action Creater
 export const textChange = (textValue, textLength) => {
 //textの数を数えるactionを定義
   return {
-    type: TEXTCHANGE,
+    type: TEXT_CHANGE,
     textValue: textValue,
     textLength: textLength,
   };
--- src/reducers/text.js
+++ src/reducers/text.js
@@ -1,10 +1,10 @@
-import {TEXTCHANGE} from '../actions';
+import {TEXT_CHANGE} from '../actions';
 
 const initialState = {textLength: 0, textValue: 'initial value'};
 //state,actionから、新しいstateを作成してreturnで返している。
 export default (state = initialState, action) => {
   switch (action.type) {
-    case TEXTCHANGE:
+    case TEXT_CHANGE:
       return {textValue: action.textValue, textLength: action.textLength};
     default:
       return state;
